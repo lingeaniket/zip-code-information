0ccada650abd0928aab03a46e05a709fbe5b42b8
lingeaniket <linge.aniket.10@gmail.com>
2023-11-03 14:15:24 +0530

    loader folder updated

--- src/Components/DisplayInformation.js
+++ src/Components/DisplayInformation.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Loader from "../Loader/Loader";
+import Loader from "./Loader";
 import SearchResults from "./SearchResults";
 
 import "../Styles/displayInformation.css";
